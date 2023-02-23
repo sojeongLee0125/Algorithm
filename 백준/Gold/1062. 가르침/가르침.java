b88e59d775f5f0c6e425b7fbc4257492d818a24e
sojeongLee <sojeonglee0125@gmail.com>
2023-02-24 07:09:38 +0900

    [Gold IV] Title: 가르침, Time: 268 ms, Memory: 15340 KB -BaekjoonHub

--- 백준/Gold/1062. 가르침/가르침.java
+++ 백준/Gold/1062. 가르침/가르침.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 /**
@@ -12,7 +11,7 @@ import java.util.StringTokenizer;
  */
 public class Main {
 
-    static int N, K, cnt;
+    static int N, K;
     static int[] words;
 
     public static void main(String[] args) throws IOException {
