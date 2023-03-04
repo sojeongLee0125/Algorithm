5f4379e4dc3110ba37ea830698787d15503cdd35
sojeongLee <sojeonglee0125@gmail.com>
2023-03-04 09:43:21 +0900

    [Silver IV] Title: 동전 0, Time: 124 ms, Memory: 14212 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.java
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -9,6 +9,17 @@ import java.util.StringTokenizer;
  * Q. 필요한 동전 개수의 최솟값을 구하는 프로그램을 작성하시오.
  * - 준규가 가지고 있는 동전은 총 N 종류, 각각의 동전을 매우 많이 가지고 있다.
  * - 동전을 적절히 사용해서 그 가치의 합을 K로 만들려고 한다.
+ * <p>
+ * 1. 아이디어
+ * - 동전을 저장한 뒤 뒤집는다.
+ * - 동전 반복문
+ * - 동전 사용 갯수 추가 및 사용한만큼 k 갱신
+ * <p>
+ * 2. 시간복잡도
+ * - O(N)
+ * <p>
+ * 3. 자료구조
+ * - 동전 금액 integer list
  */
 public class Main {
 
@@ -23,8 +34,7 @@ public class Main {
 
         // 둘째 줄부터 N개의 줄에 동전의 가치 Ai가 오름차순으로 주어진다. (1 ≤ Ai ≤ 1,000,000, A1 = 1, i ≥ 2인 경우에 Ai는 Ai-1의 배수)
         while (N-- > 0) {
-            int coin = Integer.parseInt(br.readLine());
-            if (coin <= K) coins.add(coin);
+            coins.add(Integer.parseInt(br.readLine()));
         }
 
         coins.sort(Comparator.reverseOrder());
@@ -33,11 +43,9 @@ public class Main {
         // 동전을 내림차순 정렬한다.
         // 큰 단위 가격부터 K로 나누고 그 나머지를 K 로 리정한다.
         int cnt = 0;
-        while (K != 0) {
-            for (int coin : coins) {
-                cnt += (K / coin);
-                K = K % coin;
-            }
+        for (int coin : coins) {
+            cnt += (K / coin);
+            K = K % coin;
         }
 
         // 첫째 줄에 K원을 만드는데 필요한 동전 개수의 최솟값을 출력한다.
