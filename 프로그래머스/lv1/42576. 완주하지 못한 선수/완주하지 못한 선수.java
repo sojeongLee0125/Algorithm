76d3218d58908126d1220ff2725f1625451090a4
sojeongLee <sojeonglee0125@gmail.com>
2023-03-06 11:22:40 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 88.34 ms, Memory: 111 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,29 +1,27 @@
 import java.util.*;
 
+// 단 한 명의 선수를 제외하고는 모든 선수가 마라톤을 완주하였습니다.
+// 마라톤에 참여한 선수들의 이름이 담긴 배열 participant와 완주한 선수들의 이름이 담긴 배열 completion이 주어질 때, 
 // 완주하지 못한 선수의 이름을 return 하도록 solution 함수를 작성해주세요.
-// 참가자 중에는 동명이인이 있을 수 있습니다.
+
 class Solution {
-    
-    HashMap<String, Integer> map = new HashMap<>();
-    
     public String solution(String[] participant, String[] completion) {
+        HashMap<String, Integer> map = new HashMap<>();
+        
         String answer = "";
-        
-        for(int i=0; i<participant.length; i++){
-            map.put(participant[i], map.getOrDefault(participant[i], 0) + 1);
+        // 1. 참가자 명단을 차례대로 HashMap에 저장한다. 값은 갯수.
+        for(String name : participant){
+            map.put(name, map.getOrDefault(name, 0) + 1);
+        }
+        // 2. 완주자 명단을 차례대로 HashMap에서 제거한다. 갯수--
+        for(String name : completion){
+            map.put(name, map.getOrDefault(name, 0) - 1);
+             // 3. 갯수가 0이 되면 해당 key를 삭제한다.
+            if(map.get(name) == 0) map.remove(name);
         }
         
-        for(int i=0; i<completion.length; i++){
-            if(map.containsKey(completion[i])) {
-                map.put(completion[i], map.get(completion[i]) - 1);
-                if(map.get(completion[i]) == 0) map.remove(completion[i]);
-            }
-        }
-        
-        for(String name : map.keySet()){
-            if(map.containsKey(name)) answer = name;
-        }
-        
+        for(String key : map.keySet()) answer = key;
+      
         return answer;
     }
 }
