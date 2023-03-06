7bab350956628720ed3bbd3a9ecdfd2f429ad4f2
sojeongLee <sojeonglee0125@gmail.com>
2023-03-06 11:30:33 +0900

    [level 2] Title: 전화번호 목록, Time: 198.66 ms, Memory: 134 MB -BaekjoonHub

--- 프로그래머스/lv2/42577. 전화번호 목록/README.md
+++ 프로그래머스/lv2/42577. 전화번호 목록/README.md
@@ -4,7 +4,7 @@
 
 ### 성능 요약
 
-메모리: 144 MB, 시간: 267.11 ms
+메모리: 134 MB, 시간: 198.66 ms
 
 ### 구분
 
@@ -75,8 +75,6 @@
 <p><strong>알림</strong></p>
 
 <p>2021년 3월 4일, 테스트 케이스가 변경되었습니다. 이로 인해 이전에 통과하던 코드가 더 이상 통과하지 않을 수 있습니다.</p>
-
-<p><a href="https://ncpc.idi.ntnu.no/ncpc2007/ncpc2007problems.pdf" target="_blank" rel="noopener">출처</a></p>
 
 
 > 출처: 프로그래머스 코딩 테스트 연습, https://programmers.co.kr/learn/challenges
--- 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.java
@@ -1,31 +1,29 @@
 import java.util.*;
 
 // 전화번호부에 적힌 전화번호 중, 한 번호가 다른 번호의 접두어인 경우가 있는지 확인하려 합니다.
-// 어떤 번호가 다른 번호의 접두어인 경우가 있으면 false를 그렇지 않으면 true를 return 하도록 solution 함수를 작성해주세요.
+// 어떤 번호가 다른 번호의 접두어인 경우가 있으면 false를 
+// 그렇지 않으면 true를 return 하도록 solution 함수를 작성해주세요.
+
 class Solution {
-    HashMap<String, Integer> map = new HashMap<>();
-    
     public boolean solution(String[] phone_book) {
-        boolean answer = true;
+        HashMap<String, Integer> map = new HashMap<>();
         
-        // 입력받은 전화번호는 hashmap에 넣는다.
+        boolean answer = true;
+        // 전화번호 리스트를 전부 Hash key로 저장한다.
         for(String num : phone_book){
             map.put(num, 1);
         }
         
-        outer : 
-        for(String num : phone_book){
-            map.remove(num);
-            StringBuilder sb = new StringBuilder();
-            for(int i=0; i < num.length(); i++){
-                sb.append(num.charAt(i));
-                if(map.containsKey(sb.toString())){
+        // 전화번호를 1개씩 돌면서 인덱스 0 부터 글자끝까지 map.containsKey()로 검사한다.
+        outer : for(String num : phone_book){
+            for(int i=0; i<num.length(); i++){
+                String sub = num.substring(0, i);
+                if(map.containsKey(sub)) {
                     answer = false;
                     break outer;
                 }
             }
-            map.put(num, 1);
-        }        
+        }
         
         return answer;
     }
