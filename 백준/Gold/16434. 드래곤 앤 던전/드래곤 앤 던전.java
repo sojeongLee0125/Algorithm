34056eb548ba9d0c34fddd50a122b89bd70bfec9
sojeongLee <sojeonglee0125@gmail.com>
2023-04-03 08:18:25 +0900

    [Gold IV] Title: 드래곤 앤 던전, Time: 440 ms, Memory: 49812 KB -BaekjoonHub

--- 백준/Gold/16434. 드래곤 앤 던전/드래곤 앤 던전.java
+++ 백준/Gold/16434. 드래곤 앤 던전/드래곤 앤 던전.java
@@ -1,79 +1,54 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 /**
  * Q. 용사는 N번 방에 있는 용을 쓰러트리기 위한 최소의 HMaxHP를 여러분이 계산해주면 좋겠다고 합니다.
  */
 public class Main {
-    static int N, initA;
-    static long lt, rt, max = Long.MAX_VALUE;
-    static ArrayList<long[]> rooms = new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
         // 첫 번째 줄에 방의 개수 N (1 ≤ N  ≤ 123,456) 과 용사의 초기 공격력 HATK (1 ≤ HATK  ≤ 1,000,000) 가 주어집니다.
-        N = Integer.parseInt(st.nextToken());
-        initA = Integer.parseInt(st.nextToken());
+        long N = Integer.parseInt(st.nextToken());
+        long initA = Integer.parseInt(st.nextToken());
 
         // i+1번째 줄엔 i번째 방의 정보를 나타내는 세개의 정수 ti, ai, hi (ti ∈ {1, 2}, 1 ≤ ai, hi  ≤ 1,000,000) 가 주어집니다.
+        // 모든 몬스터의 생명력 + 1 이 용사가 가질 수 있는 최소의 생명력
+        // 포션의 경우도 고려해주어야 한다. -> 포션은 다음 몬스터의 생명력을 깎는 방식으로 진행
+        long cur = 0L;
+        long p = 0L;
+
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine(), " ");
-            long t = Long.parseLong(st.nextToken());
-            long a = Long.parseLong(st.nextToken());
-            long h = Long.parseLong(st.nextToken());
-            rooms.add(new long[]{t, a, h});
-        }
+            long t = Long.parseLong(st.nextToken()); // 1 - 몬스터, 2 - 포션
+            long a = Long.parseLong(st.nextToken()); // 공격력
+            long h = Long.parseLong(st.nextToken()); // 생명력
 
-        // 이분탐색으로 용사의 최대 HP를 찾아 나간다.
-        lt = 1L;
-        rt = 1_000_000_000_000_000_000L;
-
-        while (lt <= rt) {
-            long mid = (lt + rt) / 2;
-            if (isOk(mid)) {
-                rt = mid - 1;
-                max = Math.min(max, mid);
-            } else {
-                lt = mid + 1;
-            }
-        }
-
-        // 용사가 N번째 방에 있는 용을 쓰러트리기 위한 최소의 HMaxHP를 출력합니다.
-        System.out.println(max);
-    }
-
-    private static boolean isOk(long mid) {
-        long hp = mid;
-        long atk = initA;
-
-        // 방을 처음부터 N까지 순회한다.
-        // 1이 나오면 몬스터고 2가 나오면 포션이다.
-        // 몬스터가 나오면 해당 몬스터의 생명력 / 공격력 만큼의 공격을 수행하고 그 -1번만큼 공격을 받는다.
-        // 포션이 나오면 생명력과 공격력이 올라가는데 이때 최대 올라갈 수 있는 생명력은 hp만큼만이다.
-        for (int i = 0; i < rooms.size(); i++) {
-            long[] room = rooms.get(i);
-
-            long t = room[0]; // 1 - 몬스터, 2 - 포션
-            long a = room[1]; // 공격력
-            long h = room[2]; // 생명력
-
+            //몬스터일 경우
+            // 몬스터 공격력 * 공격횟수만큼의 체력이 필요하다.
             if (t == 1L) {
-                long times = (h / atk) + (h % atk == 0 ? 0 : 1); // 총 공격 횟수
-                long att = times - 1; // 공격 받은 횟수
-                hp -= (att * a);
+                long times = (h / initA) + (h % initA == 0 ? 0 : 1);
+                long attk = times - 1;
+                long damage = (attk * a);
+
+                // 포션보다 데미지가 클 경우
+                if (p < damage) {
+                    cur += damage - p;
+                    p = 0;
+                } else p -= damage; // 포션이 몬스터 총 데미지보다 큰 경우 cur에 추가하지 않고 포션에서 빼주기만 한다.
             } else {
-                hp = Math.min(mid, hp + h);
-                atk += a;
+                //포션일 경우 - 다음 몬스터의 공격력에서 해당만큼을 뺀다.
+                initA += a;
+                p = Math.min(cur, p + h);
             }
-            if (hp <= 0) return false;
         }
 
-        return true;
+        System.out.println(cur + 1); // 총 데미지보다 1 많아아 한다.
     }
+
 
 }
