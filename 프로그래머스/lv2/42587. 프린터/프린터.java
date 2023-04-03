ec052a40da46363f4ba02ed2122bdcfa09d6401c
sojeongLee <sojeonglee0125@gmail.com>
2023-04-03 20:33:25 +0900

    [level 2] Title: 프린터, Time: 0.57 ms, Memory: 77.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42587. 프린터/프린터.java
+++ 프로그래머스/lv2/42587. 프린터/프린터.java
@@ -1,71 +1,48 @@
 import java.util.*;
 
-// 중요도가 높은 문서를 먼저 인쇄하는 프린터를 개발했습니다.
-// 인쇄 대기목록의 가장 앞에 있는 문서(J)를 대기목록에서 꺼냅니다.
-// 나머지 인쇄 대기목록에서 J보다 중요도가 높은 문서가 한 개라도 존재하면 J를 대기목록의 가장 마지막에 넣습니다.
-// 그렇지 않으면 J를 인쇄합니다.
-
-// 현재 대기목록에 있는 문서의 중요도가 순서대로 담긴 배열 priorities와 
-// 내가 인쇄를 요청한 문서가 현재 대기목록의 어떤 위치에 있는지를 알려주는 location이 매개변수로 주어질 때, 
-// 내가 인쇄를 요청한 문서가 몇 번째로 인쇄되는지 return 하도록 solution 함수를 작성해주세요.
-
 class Solution {
-    
-    static class Paper{
-        int idx;
-        int p;
-        
-        public Paper(int idx, int p){
-            this.idx = idx;
-            this.p = p;
-        }
-    }
-    
     public int solution(int[] priorities, int location) {
-        Deque<Paper> dq1 = new ArrayDeque<>();
-        Deque<Paper> dq2 = new ArrayDeque<>();
+         Deque<int[]> q1 = new ArrayDeque<>();
+         Deque<int[]> q2 = new ArrayDeque<>();
         
         for(int i=0; i<priorities.length; i++){
-            dq1.addLast(new Paper(i, priorities[i]));
+            q1.addLast(new int[]{priorities[i], i}); // 중요도 + 순서 순으로 저장
         }
         
-        // 1. paper 클래스를 정의한다. - idx, prior
-        // 2. 큐 2개를 준비한다.
-        // 3. priorities 를 paper 인스턴스로 변환하여 첫번째 큐에 순서대로 넣는다.
-        
-        // 4. 큐에서 첫번째 값을 꺼낸다. 값을 임시로 저장하고 두번째 큐에 넣는다.
-        // 5. 다음 큐부터 차례대로 꺼내서 만약 임시값보다 중요도가 큰 값이 존재하면 두번째 큐 값들을 첫번째 큐에 넣는다.
-        // 6. 만약 큐가 빌 때까지 큰 값이 없으면 두번째 큐에서 첫번째 값을 빼서 인쇄하는데 이때 location 값과 비교한다.
-        
+        // q1에서 하나를 빼서 q2로 차근차근 옮기면서 q1에서 자기보다 큰 중요도가 있는지 비교한다.
+        // q1이 빌때까지 없으면 인쇄한다.
         int cnt = 0;
         
-        while(true){
-            Paper p = dq1.pollFirst();
-            int cur = p.p;
-            dq2.addFirst(p);
+        outer: while(true){
+            int[] cur = q1.pollFirst();
+            int p = cur[0]; // 현재 꺼낸 문서의 중요도
             
-            while(!dq1.isEmpty()){
-                Paper nxt = dq1.pollFirst();
-                if(nxt.p > cur){
-                    // 뒤에 더 큰 중요도가 있는 경우
-                    dq1.addFirst(nxt);
-                    while(!dq2.isEmpty()) dq1.addLast(dq2.pollFirst());
+            // 일단 현재 문서를 q2 제일 앞단에 넣는다.
+            q2.addFirst(cur);
+            
+            while(!q1.isEmpty()){
+                int[] nx = q1.pollFirst();
+                if(nx[0] > p){
+                    // 뒤에 더 중요한 문서가 있을 경우
+                    q1.addFirst(nx);
+                    // q2에 있는 것들 전부 q1에 다시 넣고 break;
+                    while(!q2.isEmpty()) q1.addLast(q2.pollFirst());
                     break;
                 }else{
-                    // cur 보다 중요도가 작은 경우
-                    dq2.addLast(nxt);
+                    // 더 중요하지 않은 경우
+                    q2.addLast(nx);
                 }
             }
             
-            // p의 중요도가 가장 높을 경우.
-            // 7. 인쇄를 할 때 마다 cnt++ 해주고 location 값과 일치할 경우 return
-            if(dq1.isEmpty()){
-              Paper out = dq2.pollFirst();
-              cnt++;
-              if(out.idx == location) break;
+            if(q1.isEmpty()){
+                //q2의 제일 앞단 문서가 가장 중요도가 놓은 경우
+                cnt++;
+                int[] out = q2.pollFirst();
+                if(out[1] == location) break outer;
             }
             
-            while(!dq2.isEmpty()) dq1.addLast(dq2.pollFirst());
+            // q2 -> 다시 q1으로 집어넣는다.
+            while(!q2.isEmpty()) q1.addLast(q2.pollFirst()); 
         }
         
         return cnt;
