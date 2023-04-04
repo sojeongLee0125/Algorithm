b44f523327f954969f33a25bfdd2ca36cfda68c4
sojeongLee <sojeonglee0125@gmail.com>
2023-04-04 20:10:25 +0900

    [level 2] Title: H-Index, Time: 0.36 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/lv2/42747. H－Index/H－Index.java
+++ 프로그래머스/lv2/42747. H－Index/H－Index.java
@@ -1,27 +1,32 @@
 import java.util.*;
 
-// 논문 n편 중, h번 이상 인용된 논문이 h편 이상이고 나머지 논문이 h번 이하 인용되었다면 h의 최댓값이 이 과학자의 H-Index
-// 어떤 과학자가 발표한 논문의 인용 횟수를 담은 배열 citations가 매개변수로 주어질 때, 
-// 이 과학자의 H-Index를 return 하도록 solution 함수를 작성해주세요.
-
 class Solution {
+    
+    public boolean isOk(int cnt, int[] arr){
+        int up = 0;
+        int down = 0;
+        
+        for(int i=0; i<arr.length; i++){
+            if(arr[i] >= cnt) up++;
+            else down++;
+        }
+        
+        return (up >= cnt) && (down <= cnt);
+    }
+    
     public int solution(int[] citations) {
         int answer = 0;
-        // 1. citations를 오름차순 정렬한다.
-        // 2. h를 citations.length 부터 작아지면서 진행한다.
-        // 3. 만족하는 h가 있다면 break;
+        int lt = 0;
+        int rt = 10000;
         
         Arrays.sort(citations);
         
-        for(int h = citations.length; h >= 0; h--){
-            int cnt = 0;
-            for(int x : citations){
-                if(x >= h) cnt++;
-            }
-            if(cnt >= h) {
-                answer = h;
-                break;
-            }
+        while(lt <= rt){
+            int mid = (lt + rt) / 2;
+            if(isOk(mid, citations)){
+                answer = mid;
+                lt = mid + 1;
+            }else rt = mid - 1;
         }
         
         return answer;
