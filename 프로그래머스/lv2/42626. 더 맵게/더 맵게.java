6895acd8efa63695201a4624be76c71c1af0e436
sojeongLee <sojeonglee0125@gmail.com>
2023-04-04 14:20:11 +0900

    [level 2] Title: 더 맵게, Time: 1506.93 ms, Memory: 122 MB -BaekjoonHub

--- 프로그래머스/lv2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/lv2/42626. 더 맵게/더 맵게.java
@@ -1,38 +1,30 @@
 import java.util.*;
 
-
-//  모든 음식의 스코빌 지수를 K 이상으로 만들고 싶습니다.
-// 스코빌 지수가 가장 낮은 두 개의 음식을 아래와 같이 특별한 방법으로 섞어 새로운 음식을 만듭니다.
-// 섞은 음식의 스코빌 지수 = 가장 맵지 않은 음식의 스코빌 지수 + (두 번째로 맵지 않은 음식의 스코빌 지수 * 2)
-// 모든 음식의 스코빌 지수가 K 이상이 될 때까지 반복하여 섞습니다.
-
-// 모든 음식의 스코빌 지수를 K 이상으로 만들기 위해 섞어야 하는 최소 횟수를 return 
-// 모든 음식의 스코빌 지수를 K 이상으로 만들 수 없는 경우에는 -1을 return 합니다.
-
 class Solution {
+    PriorityQueue<Integer> pq = new PriorityQueue<>();
+    
     public int solution(int[] scoville, int K) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-        if(K == 0) return 0;
-        if(scoville.length < 2) return -1;
-        
         int answer = 0;
         
-        // 1. PriorityQueue에 scoville 지수들을 넣는다.
-        for(int n : scoville){
-            pq.add(n);
+        for(int n: scoville) pq.add(n);
+        
+        while(!pq.isEmpty()){
+            if(pq.size() <= 1 && pq.peek() < K){
+                answer = -1;
+                break;
+            }
+            
+            // 제일 안매운 요소가 >= K 인지 검사
+            int c1 = pq.poll();
+            if(c1 >= K) break;
+            else{
+                int c2 = pq.poll();
+                int n = c1 + (c2 * 2);
+                pq.add(n);
+                answer++;
+            }
         }
         
-        // 2. 첫번째와 두번째 요소를 꺼내는데 이 때 첫번째 요소가 K 이상일 경우 break 한다. 
-        while(pq.size() > 1 && pq.peek() < K){
-            int first = pq.poll();
-            int second = pq.poll();
-            int sum = first + (second * 2);
-            pq.add(sum);
-            answer++;
-        }
-        
-        if(pq.size() >= 1 && pq.peek() >= K){
-            return answer;
-        }else return -1;
+        return answer;
     }
 }
