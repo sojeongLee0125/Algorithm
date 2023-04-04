6c5e0fbf93c4688ef11bfe7c8c0eed833dd863f2
sojeongLee <sojeonglee0125@gmail.com>
2023-04-04 19:16:09 +0900

    [level 1] Title: K번째수, Time: 0.39 ms, Memory: 78.3 MB -BaekjoonHub

--- 프로그래머스/lv1/42748. K번째수/K번째수.java
+++ 프로그래머스/lv1/42748. K번째수/K번째수.java
@@ -1,26 +1,19 @@
 import java.util.*;
 
 class Solution {
-    ArrayList<Integer> ans = new ArrayList<>();
     
     public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+        int idx = 0;
         
         for(int[] arr : commands){
-            ArrayList<Integer> tmp = new ArrayList<>();
-            int i = arr[0] - 1;
-            int j = arr[1] - 1;
-            int k = arr[2] -1;
-            for(int s = i; s <= j; s++){
-                tmp.add(array[s]);
-            }
-            tmp.sort(Integer::compareTo);
-            ans.add(tmp.get(k));
-        }
-        
-        int[] answer = new int[ans.size()];
-        
-        for(int i=0; i<answer.length; i++){
-            answer[i] = ans.get(i);
+            int i = arr[0]; 
+            int j = arr[1];
+            int k = arr[2];
+            
+            int[] tmp = Arrays.copyOfRange(array, i-1, j);
+            Arrays.sort(tmp);
+            answer[idx++] = tmp[k-1];
         }
         
         return answer;
