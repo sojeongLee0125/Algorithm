c9af32de5e29ec2f0289ec06d6465bbdbeb94237
sojeongLee <sojeonglee0125@gmail.com>
2023-04-04 21:22:33 +0900

    [level 2] Title: 소수 찾기, Time: 0.76 ms, Memory: 77.5 MB -BaekjoonHub

--- 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.java
@@ -1,54 +1,51 @@
 import java.util.*;
 
-// 한자리 숫자가 적힌 종이 조각이 흩어져있습니다. 흩어진 종이 조각을 붙여 소수를 몇 개 만들 수 있는지 알아내려 합니다.
-// 각 종이 조각에 적힌 숫자가 적힌 문자열 numbers가 주어졌을 때, 
-// 종이 조각으로 만들 수 있는 소수가 몇 개인지 return 하도록 solution 함수를 완성해주세요.
-
 class Solution {
+    HashMap<Integer, Integer> map = new HashMap<>(); // 중복 소수 체크용
+    int[] chk = new int[10]; // 0 부터 9까지 갯수 저장 
+    int len, cnt;
     
-    int[] nums;
-    int[] chk;
-    int[] check = new int[99999999];
-    int size, rs;
+    public int solution(String numbers) {
+        len = numbers.length();
+        
+        for(char c : numbers.toCharArray()){
+            chk[c -'0']++;
+        }
+        
+        dfs(0, new StringBuilder());
+        
+        return cnt;
+    }
     
     public boolean isPrime(String num){
-        int number = Integer.parseInt(num);
-        if(number < 2) return false;
-        for(int i=2; i <= (int) Math.sqrt(number); i++){
-            if(number % i == 0) return false;
+        if(num.equals("")) return false;
+        
+        int n = Integer.parseInt(num);
+        
+        if(n < 2) return false;
+        for(int i=2; i<=Math.sqrt(n); i++){
+            if(n % i == 0) return false;
         }
+        
         return true;
     }
     
-    public void go(String str){
-        for(int i=0; i<size; i++){
-            if(chk[i] == 0){
-                chk[i] = 1;
-                String tmp = str + nums[i];
-                if(check[Integer.parseInt(tmp)] != 1) if(isPrime(tmp)) rs++;
-                // tmp 중복된 숫자는 제거
-                check[Integer.parseInt(tmp)] = 1;
-                go(tmp);
-                chk[i] = 0;
+    public void dfs(int idx, StringBuilder sb){
+        if(idx > len) return;
+    
+        if(isPrime(sb.toString()) 
+           && !map.containsKey(Integer.parseInt(sb.toString()))){
+            map.put(Integer.parseInt(sb.toString()), 1);
+            cnt++;
+        }
+        
+        for(int i=0; i<10; i++){
+            if(chk[i] > 0){
+                chk[i]--;
+                dfs(idx + 1, sb.append(i));
+                sb.deleteCharAt(sb.toString().length()-1);
+                chk[i]++;
             }
         }
-    }
-    
-    public int solution(String numbers) {
-        int answer = 0;
-        size = numbers.length();
-        nums = new int[size];
-        chk = new int[size];
-        
-        // 숫자를 배열로 만든다.
-        // 배열의 순열을 구한다.
-        for(int i=0; i < numbers.length(); i++){
-            nums[i] = numbers.charAt(i) - '0';
-        }
-        
-        go("");
-        
-        // 해당 값이 소수라면 정답에 체크한다.
-        return rs;
     }
 }
