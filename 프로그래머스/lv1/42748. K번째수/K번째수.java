fe7a54c4deef5bc5e1883a744141986d66fbcd56
sojeongLee <sojeonglee0125@gmail.com>
2023-04-04 19:12:24 +0900

    [level 1] Title: K번째수, Time: 0.61 ms, Memory: 78.5 MB -BaekjoonHub

--- 프로그래머스/lv1/42748. K번째수/K번째수.java
+++ 프로그래머스/lv1/42748. K번째수/K번째수.java
@@ -1,21 +1,26 @@
 import java.util.*;
 
-// 배열 array의 i번째 숫자부터 j번째 숫자까지 자르고 정렬했을 때, k번째에 있는 수를 구하려 합니다.
-// 배열 array, [i, j, k]를 원소로 가진 2차원 배열 commands가 매개변수로 주어질 때, 
-// 결과를 배열에 담아 return 하도록 solution 함수를 작성해주세요.
 class Solution {
+    ArrayList<Integer> ans = new ArrayList<>();
+    
     public int[] solution(int[] array, int[][] commands) {
-        int[] answer = new int[commands.length];
-        int idx = 0;
         
-        // commands 를 반복문으로 돌면서 맞는 값을 구한다.
-        for(int i=0; i<commands.length; i++){
-            int st = commands[i][0] - 1;
-            int ed = commands[i][1];
-            int k = commands[i][2] - 1;
-            int[] arr2 = Arrays.copyOfRange(array, st, ed);
-            Arrays.sort(arr2);
-            answer[idx++] = arr2[k];
+        for(int[] arr : commands){
+            ArrayList<Integer> tmp = new ArrayList<>();
+            int i = arr[0] - 1;
+            int j = arr[1] - 1;
+            int k = arr[2] -1;
+            for(int s = i; s <= j; s++){
+                tmp.add(array[s]);
+            }
+            tmp.sort(Integer::compareTo);
+            ans.add(tmp.get(k));
+        }
+        
+        int[] answer = new int[ans.size()];
+        
+        for(int i=0; i<answer.length; i++){
+            answer[i] = ans.get(i);
         }
         
         return answer;
