4ce16e491b8bc383eb0250f72edde8afea356fd2
sojeongLee <sojeonglee0125@gmail.com>
2023-04-04 09:41:52 +0900

    [Platinum V] Title: 가장 긴 증가하는 부분 수열 5, Time: 5408 ms, Memory: 278428 KB -BaekjoonHub

--- 백준/Platinum/14003. 가장 긴 증가하는 부분 수열 5/가장 긴 증가하는 부분 수열 5.java
+++ 백준/Platinum/14003. 가장 긴 증가하는 부분 수열 5/가장 긴 증가하는 부분 수열 5.java
@@ -1,60 +1,70 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Stack;
+import java.util.StringTokenizer;
 
-public class Main{
-
+/**
+ * Q. 수열 A가 주어졌을 때, 가장 긴 증가하는 부분 수열을 구하는 프로그램을 작성하시오.
+ */
+public class Main {
     static int N;
+    static int[] arr;
+    static int[] idxArr; // 경로 추적
+    static ArrayList<Integer> sizeList = new ArrayList<>(); // 사이즈 저장
 
-    public static void main(String[] args)throws IOException {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        ArrayList<Integer> listForSize = new ArrayList<>();
-        listForSize.add(Integer.MIN_VALUE);
+        // 최솟값 먼저 저장
+        sizeList.add(Integer.MIN_VALUE);
 
+        // 수열 A의 크기 N (1 ≤ N ≤ 1,000,000)이 주어진다.
         N = Integer.parseInt(br.readLine());
-        int[] arr = new int[N];
-        int[] indexs = new int[N];
+        arr = new int[N];
+        idxArr = new int[N];
 
-        st = new StringTokenizer(br.readLine(), " ");
-        for(int i = 0; i < N; i++) {
+        //둘째 줄에는 수열 A를 이루고 있는 Ai가 주어진다. (-1,000,000,000 ≤ Ai ≤ 1,000,000,000)
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        for(int i = 0; i < N; i++) {
-            if(listForSize.get(listForSize.size()-1) < arr[i]) {
-                listForSize.add(arr[i]);
-                indexs[i] = listForSize.size() - 1;
-            }
-            else {
-                int left = 1;
-                int right = listForSize.size()-1;
+        for (int i = 0; i < N; i++) {
+            // 리스트 마지막보다 큰 경우 리스트 마지막에 삽입
+            if (sizeList.get(sizeList.size() - 1) < arr[i]) {
+                sizeList.add(arr[i]);
+                idxArr[i] = sizeList.size() - 1;
+            } else {
+                // 그 외의 경우 이분탐색으로 lowerBound 위치 찾아서 해당 위치 숫자와 교체
+                int lt = 1;
+                int rt = sizeList.size() - 1;
 
-                while (left < right) {
-                    int mid = (left + right) / 2;
-                    if(listForSize.get(mid) < arr[i]) {
-                        left = mid + 1;
-                    }else {
-                        right = mid;
+                while (lt < rt) {
+                    int mid = (lt + rt) / 2;
+                    if (sizeList.get(mid) < arr[i]) {
+                        lt = mid + 1;
+                    } else {
+                        rt = mid;
                     }
                 }
-                listForSize.set(right, arr[i]);
-                indexs[i] = right;
-            }
-        }
-        System.out.println(listForSize.size() - 1);
-
-        int index = listForSize.size() - 1;
-        Stack<Integer> stack = new Stack<>();
-        for(int i = N-1; i >= 0; i--) {
-            if(indexs[i] == index) {
-                index--;
-                stack.push(arr[i]);
+                sizeList.set(rt, arr[i]);
+                idxArr[i] = rt;
             }
         }
 
-        while (!stack.isEmpty()) {
-            System.out.print(stack.pop() + " ");
+        // 첫째 줄에 수열 A의 가장 긴 증가하는 부분 수열의 길이를 출력한다.
+        int idx = sizeList.size() - 1;
+        System.out.println(idx);
+
+        Stack<Integer> s = new Stack<>();
+        for (int i = N - 1; i >= 0; i--) {
+            if (idxArr[i] == idx) {
+                idx--;
+                s.push(arr[i]);
+            }
         }
-        System.out.println();
+
+        while (!s.isEmpty()) System.out.print(s.pop() + " ");
     }
 }
