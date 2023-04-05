3fb912a6e8cd963f72d3df4969d3cd970c48fed7
sojeongLee <sojeonglee0125@gmail.com>
2023-04-05 21:23:48 +0900

    [level 2] Title: 조이스틱, Time: 0.06 ms, Memory: 70.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42860. 조이스틱/조이스틱.java
+++ 프로그래머스/lv2/42860. 조이스틱/조이스틱.java
@@ -2,19 +2,33 @@ import java.util.*;
 
 class Solution {
     public int solution(String name) {
+        int len = name.length();
+        int move = len - 1;
         int answer = 0;
-        int move = name.length() - 1; // 오른쪽으로만 직진
         
-        for(int i=0; i<name.length(); i++){
-            answer += Math.min(name.charAt(i) - 'A', 'Z' - name.charAt(i) + 1);
-            // A가 등장할 경우
-            if(i < name.length() - 1 && name.charAt(i + 1) == 'A'){
-                int lastA = i + 1;
-                while(lastA < name.length() && name.charAt(lastA) == 'A') lastA++;
-                move = Math.min(move, i * 2 + (name.length() - lastA));
-                move = Math.min(move, i + (name.length() - lastA) * 2);
+        
+        // 조이스틱을 움직이는 3가지 경우
+        // 1. ABDCERT 직진만 하는 경우
+        // 2. CDAAAAAAAD 직진하다가 다시 돌아와서 역으로 가는 경우
+        // 3. BADFAAAAAFD 역행하다가 다시 돌아와서 직진으로 가는 경우
+        // 각 알파벳의 그 자리 움직임 + 총 MOVE 수
+        
+        for(int i=0; i<len; i++){
+            char c = name.charAt(i);
+            answer += Math.min(c - 'A', 'Z' - c + 1);
+            
+            if(i < len-1 && name.charAt(i+1) == 'A'){
+                int endA = i+1;
+                
+                while(endA < len && name.charAt(endA) == 'A'){
+                    endA++;
+                }
+                
+                move = Math.min(move, (i*2) + len - endA); // 직진하다가 돌아와서 역행하는 경우
+                move = Math.min(move, (i + (len - endA) * 2) ); // 역행 후 돌아와서 다시 직진하는 경우
             }
         }
+        
         
         return answer + move;
     }
