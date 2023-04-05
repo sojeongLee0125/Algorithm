3cdb687a3cec1730884521cd6f95c6f873fcd172
sojeongLee <sojeonglee0125@gmail.com>
2023-04-05 19:50:31 +0900

    [level 2] Title: 모음 사전, Time: 2.96 ms, Memory: 69.6 MB -BaekjoonHub

--- 프로그래머스/lv2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/lv2/84512. 모음 사전/모음 사전.java
@@ -2,29 +2,20 @@ import java.util.*;
 
 class Solution {
     String target;
-    int ret;
     String[] arr = {"A","E","I","O","U"};
     ArrayList<String> list = new ArrayList<>();
     
     public int solution(String word) {
         target = word;
         dfs(0, new StringBuilder(), 0);
-        
-        for(int i=0; i<list.size(); i++){
-            if(list.get(i).equals(target)) break;
-            ret++;
-        }
-        
-        return ret;
+        return list.indexOf(target);
     }
     
     public void dfs(int idx, StringBuilder sb, int cnt){
         if(idx == 6) return;
         list.add(sb.toString());
-        
         for(int i=0; i<5; i++){
-            sb.append(arr[i]);
-            dfs(idx+1, sb, cnt);
+            dfs(idx+1, sb.append(arr[i]), cnt);
             sb.deleteCharAt(idx);
         }
     }
