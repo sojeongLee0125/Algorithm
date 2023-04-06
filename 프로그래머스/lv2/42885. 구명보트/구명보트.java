e90cc17ed02ec78128713a7d37d3530b72e37b0f
sojeongLee <sojeonglee0125@gmail.com>
2023-04-06 11:16:14 +0900

    [level 2] Title: 구명보트, Time: 12.86 ms, Memory: 53.9 MB -BaekjoonHub

--- 프로그래머스/lv2/42885. 구명보트/구명보트.java
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.java
@@ -1,3 +1,4 @@
+
 import java.util.*;
 
 // 구명보트는 작아서 한 번에 최대 2명씩 밖에 탈 수 없고, 무게 제한도 있습니다.
@@ -8,22 +9,22 @@ import java.util.*;
 class Solution {
     public int solution(int[] people, int limit) {
         int answer = 0;
-        
+
         // 1. 사람들을 오름차순으로 정렬한다.
         // 2. 포인터를 왼쪽 끝 (최하위 몸무게) , 오른쪽 끝 (최상위 몸무게)로 놔둔다.
         // 3. 포인터 왼쪽을 오른쪽으로 이동한다.
         //  - 오른쪽 포인터를 왼쪽으로 이동하면서 비교하여 limit 안에 들어오면 멈춘다. 
         //  - cnt를 올리고 해당 오른쪽 값은 250으로 만든다.
-        
+
         Arrays.sort(people);
-        
+
         int rt = people.length - 1;
-        
+
         for(int lt=0; lt < people.length; lt++){
             if(people[lt] == 250) break;
-            
+
             int cur = people[lt];
-            
+
             while(lt < rt){
                 if(cur + people[rt] <= limit){
                     answer++;
@@ -34,11 +35,11 @@ class Solution {
                 rt--;
             }
         }
-        
+
         for(int i=0; i < people.length; i++){
             if(people[i] != 250) answer++;
         }
-        
+
         return answer;
     }
 }
