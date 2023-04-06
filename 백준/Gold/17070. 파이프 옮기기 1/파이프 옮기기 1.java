575bed901d564d88007f53e4ac6de6e32bb20e49
sojeongLee <sojeonglee0125@gmail.com>
2023-04-06 09:40:29 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 128 ms, Memory: 14336 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
@@ -8,15 +8,14 @@ import java.util.StringTokenizer;
  */
 public class Main {
     static int N;
-    static int[][] map;
-    static int[][][] dp = new int[20][20][3]; // y, x , 가로세로대각선
+    static int[][] map = new int[20][20];
+    static int[][][] dp = new int[20][20][3]; // y좌표, x좌표, 방향값
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         // 첫째 줄에 집의 크기 N(3 ≤ N ≤ 16)이 주어진다.
         N = Integer.parseInt(br.readLine());
-        map = new int[20][20];
 
         // 둘째 줄부터 N개의 줄에는 집의 상태가 주어진다. 빈 칸은 0, 벽은 1로 주어진다. (1, 1)과 (1, 2)는 항상 빈 칸이다.
         for (int i = 0; i < N; i++) {
@@ -26,32 +25,36 @@ public class Main {
             }
         }
 
-        dp[0][1][0] = 1; // 최초지점 체크
+        // 출발점 체크
+        dp[0][1][0] = 1;
+
+        // 모든 좌표를 순차적으로 검사
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
-                // 가로의 경우
-                if(isOk(i, j+1, 0)) dp[i][j + 1][0] += dp[i][j][0]; // 가로
-                if(isOk(i+1, j+1, 2)) dp[i+1][j+1][2] += dp[i][j][0]; // 대각선
-                // 세로
-                if(isOk(i+1, j, 1)) dp[i+1][j][1] += dp[i][j][1]; // 세로
-                if(isOk(i+1, j+1, 2)) dp[i+1][j+1][2] += dp[i][j][1]; // 대각선
-                // 대각선
-                if(isOk(i, j+1, 0)) dp[i][j + 1][0] += dp[i][j][2]; // 가로
-                if(isOk(i+1, j, 1)) dp[i+1][j][1] += dp[i][j][2]; // 세로
-                if(isOk(i+1, j+1, 2)) dp[i+1][j+1][2] += dp[i][j][2]; // 대각선
+                // 가로로 진행하는 경우
+                if (isOk(i, j + 1, 0)) dp[i][j + 1][0] += dp[i][j][0]; // 가로 -> 가로
+                if (isOk(i + 1, j + 1, 2)) dp[i + 1][j + 1][2] += dp[i][j][0]; // 가로 -> 대각선
+
+                // 세로로 진행하는 경우
+                if (isOk(i + 1, j, 1)) dp[i + 1][j][1] += dp[i][j][1]; // 세로 -> 세로
+                if (isOk(i + 1, j + 1, 2)) dp[i + 1][j + 1][2] += dp[i][j][1]; // 세로 -> 대각선
+
+                // 대각선으로 진행하는 경우
+                if (isOk(i, j + 1, 0)) dp[i][j + 1][0] += dp[i][j][2]; // 대각선 -> 가로
+                if (isOk(i + 1, j, 1)) dp[i + 1][j][1] += dp[i][j][2]; // 대각선 -> 세로
+                if (isOk(i + 1, j + 1, 2)) dp[i + 1][j + 1][2] += dp[i][j][2]; // 대각선 -> 대각선
             }
         }
 
-        System.out.println(dp[N-1][N-1][0] + dp[N-1][N-1][1] + dp[N-1][N-1][2]);
+        // 첫째 줄에 파이프의 한쪽 끝을 (N, N)으로 이동시키는 방법의 수를 출력한다.
+        // 이동시킬 수 없는 경우에는 0을 출력한다. 방법의 수는 항상 1,000,000보다 작거나 같다.
+        System.out.println(dp[N - 1][N - 1][0] + dp[N - 1][N - 1][1] + dp[N - 1][N - 1][2]);
     }
 
     private static boolean isOk(int y, int x, int dir) {
-        if(dir < 2){
-            // 가로 세로일경우
-            return map[y][x] == 0;
-        }else{
-            // 대각선일 경우
-            return map[y][x] == 0 &&  map[y-1][x] == 0 && map[y][x-1] == 0;
+        if (dir < 2) return map[y][x] == 0; // 가로 세로일 경우 해당 칸만 검사
+        else {
+            return map[y][x] == 0 && map[y - 1][x] == 0 && map[y][x - 1] == 0; // 대각선일 경우 3칸 검사
         }
     }
 
