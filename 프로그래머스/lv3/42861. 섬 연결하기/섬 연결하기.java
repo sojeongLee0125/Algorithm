f7474d1999a4cedfaf70772c3fb3e9a5028ea6fd
sojeongLee <sojeonglee0125@gmail.com>
2023-04-06 11:38:31 +0900

    [level 3] Title: 섬 연결하기, Time: 0.11 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/lv3/42861. 섬 연결하기/섬 연결하기.java
+++ 프로그래머스/lv3/42861. 섬 연결하기/섬 연결하기.java
@@ -1,63 +1,45 @@
 import java.util.*;
 
-// n개의 섬 사이에 다리를 건설하는 비용(costs)이 주어질 때, 
-// 최소의 비용으로 모든 섬이 서로 통행 가능하도록 만들 때 필요한 최소 비용을 return 하도록 solution을 완성하세요.
-
 class Solution {
     
-    class Node implements Comparable<Node> {
-        int cost;
-        int num;
-        
-        public Node(int cost, int num){
-            this.cost = cost;
-            this.num = num;
-        }
-        
-        // 비용 오름차순
-        @Override
-        public int compareTo(Node o){
-            return this.cost - o.cost;
-        }
-    }
+    ArrayList<int[]>[] tree;
     
-    public int solution(int n, int[][] costs) {    
-        ArrayList<Node>[] tree = new ArrayList[n];
+    PriorityQueue<int[]> pq = new PriorityQueue<>((a1, a2) -> a1[0] - a2[0]);
+    
+    public int solution(int n, int[][] costs) {
+        int answer = 0;
         int[] chk = new int[n];
         
-        int answer = 0;
-        // Node 자료구조를 만든다.
-        // 그래프는 ArrayList<Node>[] 배열로 만든다.
-        // PriorityQueue 자료구조를 만들어서 (비용, 노드번호)를 넣는다.
-        // MST로 푼다.
+        tree = new ArrayList[n];        
+        for(int i=0; i<n; i++) tree[i] = new ArrayList<>();
         
-        for(int i=0; i<n; i++){
-            tree[i] = new ArrayList<>();
+        // 트리 구조를 만든다.
+        for(int[] arr : costs){
+            int v1 = arr[0];
+            int v2 = arr[1];
+            int w = arr[2];
+            tree[v1].add(new int[]{w, v2});
+            tree[v2].add(new int[]{w, v1});
         }
         
-        for(int i=0; i<costs.length; i++){
-            int n1 = costs[i][0];
-            int n2 = costs[i][1];
-            int cost = costs[i][2];
-            tree[n1].add(new Node(cost, n2));
-            tree[n2].add(new Node(cost, n1));
+        // 0번부터 넣는다.
+        chk[0] = 1;
+        for(int[] a : tree[0]){
+            pq.add(a);
         }
-        
-        
-        PriorityQueue<Node> pq = new PriorityQueue<>();
-        
-        // 0번 노드부터 시작
-        pq.add(new Node(0, 0));
         
         while(!pq.isEmpty()){
-            Node cur = pq.poll();
-            if(chk[cur.num] == 1) continue;
+            int[] cur = pq.poll();
             
-            chk[cur.num] = 1;
-            answer += cur.cost;
+            int cw = cur[0];
+            int nx = cur[1];
             
-            for(Node node : tree[cur.num]){
-                pq.add(node);
+            if(chk[nx] != 0) continue;
+            chk[nx] = 1;
+            answer += cw;
+            
+            for(int[] a : tree[nx]){
+                if(chk[a[1]] == 0) pq.add(a);
             }
         }
         
