dbd36183edda1379420c472e58e1255b4afb1b91
sojeongLee <sojeonglee0125@gmail.com>
2023-04-06 11:19:30 +0900

    [level 2] Title: 구명보트, Time: 17.04 ms, Memory: 53.9 MB -BaekjoonHub

--- 프로그래머스/lv2/42885. 구명보트/구명보트.java
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.java
@@ -1,45 +1,33 @@
-
 import java.util.*;
-
-// 구명보트는 작아서 한 번에 최대 2명씩 밖에 탈 수 없고, 무게 제한도 있습니다.
-// 구명보트를 최대한 적게 사용하여 모든 사람을 구출하려고 합니다.
-// 사람들의 몸무게를 담은 배열 people과 구명보트의 무게 제한 limit가 매개변수로 주어질 때, 
-// 모든 사람을 구출하기 위해 필요한 구명보트 개수의 최솟값을 return 하도록 solution 함수를 작성해주세요.
 
 class Solution {
     public int solution(int[] people, int limit) {
         int answer = 0;
-
-        // 1. 사람들을 오름차순으로 정렬한다.
-        // 2. 포인터를 왼쪽 끝 (최하위 몸무게) , 오른쪽 끝 (최상위 몸무게)로 놔둔다.
-        // 3. 포인터 왼쪽을 오른쪽으로 이동한다.
-        //  - 오른쪽 포인터를 왼쪽으로 이동하면서 비교하여 limit 안에 들어오면 멈춘다. 
-        //  - cnt를 올리고 해당 오른쪽 값은 250으로 만든다.
-
-        Arrays.sort(people);
-
         int rt = people.length - 1;
-
+        
+        // 1. 배열을 정렬한다.
+        // 2. 반복문은 lt가 -> 끝까지 진행하는 식으로 한다.
+        // 3. 각각의 lt마다 rt를 감소하며 짝을 찾는다. 이때 보트에 쌍으로 타는 사람들은 큰 수로 표시한다.
+        
+        Arrays.sort(people);
+        
         for(int lt=0; lt < people.length; lt++){
-            if(people[lt] == 250) break;
-
-            int cur = people[lt];
-
+            if(people[lt] == 300) continue;   
             while(lt < rt){
-                if(cur + people[rt] <= limit){
+                if(people[lt] + people[rt] <= limit){
+                    people[lt] = 300;
+                    people[rt] = 300;
                     answer++;
-                    people[rt] = 250;
-                    people[lt] = 250;
                     break;
                 }
                 rt--;
             }
         }
-
-        for(int i=0; i < people.length; i++){
-            if(people[i] != 250) answer++;
+        
+        for(int i=0; i<people.length; i++){
+            if(people[i] != 300) answer++;
         }
-
+        
         return answer;
     }
 }
