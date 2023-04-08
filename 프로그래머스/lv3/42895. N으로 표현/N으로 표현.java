4ee4a8e1a50aa9aa568c1def4061a27a93837953
sojeongLee <sojeonglee0125@gmail.com>
2023-04-08 09:16:55 +0900

    [level 3] Title: N으로 표현, Time: 14.11 ms, Memory: 78.8 MB -BaekjoonHub

--- 프로그래머스/lv3/42895. N으로 표현/N으로 표현.java
+++ 프로그래머스/lv3/42895. N으로 표현/N으로 표현.java
@@ -14,7 +14,7 @@ class Solution {
             num = (10 * num) + N;
         }
         
-        for(int i=1; i<9; i++){
+        for(int i=2; i<9; i++){
             for(int j=1; j<i; j++){
                 for(int a : set[j]){
                     for(int b : set[i-j]){
