814cc261e372aebeaf7599ebe3a5762b6984cd2d
sojeongLee <sojeonglee0125@gmail.com>
2023-04-18 19:52:01 +0900

    [level 4] Title: 사칙연산, Time: 21.39 ms, Memory: 53.1 MB -BaekjoonHub

--- 프로그래머스/lv4/1843. 사칙연산/사칙연산.java
+++ 프로그래머스/lv4/1843. 사칙연산/사칙연산.java
@@ -1,87 +1,77 @@
 import java.util.*;
 
+// 사칙연산에서 더하기(+)는 결합법칙이 성립하지만, 빼기(-)는 결합법칙이 성립하지 않습니다.
+// 뺄셈은 연산 순서에 따라 그 결과가 바뀔 수 있습니다.
 // 문자열 형태의 숫자와, 더하기 기호("+"), 뺄셈 기호("-")가 들어있는 배열 arr가 매개변수로 주어질 때, 
 // 서로 다른 연산순서의 계산 결과 중 최댓값을 return 하도록 solution 함수를 완성해 주세요.
 
 class Solution {
+    ArrayList<String> ops = new ArrayList<>(); // 연산자
+    ArrayList<Integer> nums = new ArrayList<>(); // 숫자
+    int[][][] dp = new int[2][205][205]; // 최대(1) or 최소(0) flag, st, ed
     
-    // 최소(0) 최대(1) / 시작 인덱스 / 끝 인덱스
-    int[][][] dp = new int[2][205][205];
-    
-    ArrayList<Integer> nums = new ArrayList<>(); // 숫자들 저장
-    ArrayList<String> ops = new ArrayList<>(); // 연산자 저장
+    public int solution(String arr[]) {
+        int answer = -1;
+        
+        // 1. 연산자와 숫자를 구분해서 저장하기
+        for(String str : arr){
+            if(str.equals("+") || str.equals("-")) ops.add(str);
+            else nums.add(Integer.parseInt(str));
+        }
+        
+        // 2. dp 배열 초기화
+        for(int i=0; i<dp.length; i++){
+            for(int j=0; j<dp[i].length; j++){
+                if(i == 0) Arrays.fill(dp[i][j], Integer.MAX_VALUE);
+                else if(i == 1) Arrays.fill(dp[i][j], Integer.MIN_VALUE);
+            }
+        }
+        
+        return sol(1, 0, nums.size() - 1);
+    }
     
     public int sol(int flag, int st, int ed){
         
-        // 범위가 일치하는 경우 = 숫자 하나인 경우
+        // 1. st == ed 일 경우 (값이 하나일 경우)
         if(st == ed){
             dp[flag][st][ed] = nums.get(st);
             return dp[flag][st][ed];
         }
         
-        // dp 값 존재하는 경우
-        if(flag == 0 && dp[flag][st][ed] != Integer.MAX_VALUE){
-            return dp[flag][st][ed];
-        }else if(flag == 1 && dp[flag][st][ed] != Integer.MIN_VALUE){
+        // 2. dp값이 존재하는 경우
+        if((flag == 0 && dp[flag][st][ed] != Integer.MAX_VALUE) || (flag == 1 && dp[flag][st][ed] != Integer.MIN_VALUE)){
             return dp[flag][st][ed];
         }
         
-        // 초기값 설정
+        // 3. rs 값 초기화
         int rs = 0;
-        if(flag == 0) rs = Integer.MAX_VALUE; // 최솟값일 경우
-        else if(flag == 1) rs = Integer.MIN_VALUE; // 최댓값일 경우
+        if(flag == 0) rs = Integer.MAX_VALUE; // 최솟값을 구할 경우 최댓값으로 초기화
+        else rs = Integer.MIN_VALUE; // 최댓값을 구할 경우 최솟값으로 초기화
         
-        // DP 갱신
-        dp[flag][st][ed] = 0;
-        
-        // 최솟값을 구하는 경우
-        if(flag == 0){
-            for(int i=st; i<ed; i++){
-                // 구간 간 - 연산자의 경우 "최소 - 최대"가 되어야 한다.
+        for(int i=st; i<ed; i++){
+            
+            // 최솟값을 구할 경우 
+            if(flag == 0){
+                // 기호가 - 일 경우 (최소 - 최대)가 되어야 한다.
                 if(ops.get(i).equals("-")){
                     rs = Math.min(rs, sol(0, st, i) - sol(1, i+1, ed));
-                }else{
-                    // 최소 + 최소
+                } // 기호가 + 일 경우 (최소 - 최소)가 되어야 한다.
+                else if(ops.get(i).equals("+")){
                     rs = Math.min(rs, sol(0, st, i) + sol(0, i+1, ed));
                 }
             }
-        }else{
-            //최댓값을 구하는 경우
-            for(int i=st; i<ed; i++){
-                // 구간 간 - 연산자의 경우 최대 - 최소가 되어야 한다.
+            // 최댓값을 구할 경우 
+            else if(flag == 1){
+                // 기호가 - 일 경우 (최대 - 최소)가 되어야 한다.
                 if(ops.get(i).equals("-")){
                     rs = Math.max(rs, sol(1, st, i) - sol(0, i+1, ed));
-                }else{
-                    //최대 + 최대
+                } // 기호가 + 일 경우 (최대 - 최대)가 되어야 한다.
+                else if(ops.get(i).equals("+")){
                     rs = Math.max(rs, sol(1, st, i) + sol(1, i+1, ed));
                 }
-            }
-        }
+            }   
+        }   
         
         return dp[flag][st][ed] = rs;
     }
-    
-    public int solution(String arr[]) {
-        
-        // 숫자/문자 배열 따로 저장
-        for(int i=0; i<arr.length; i++){
-            if(i % 2 == 0) nums.add(Integer.parseInt(arr[i]));
-            else ops.add(arr[i]);
-        }
-        
-        // dp 배열 초기화
-        for(int i=0; i < nums.size(); i++){
-            for(int j=0; j < nums.size(); j++){
-                // 최솟값
-                dp[0][i][j] = Integer.MAX_VALUE;
-                // 최댓값 
-                dp[1][i][j] = Integer.MIN_VALUE;
-            }
-        }
-        
-        int answer = sol(1, 0, nums.size() - 1);
-        
-        return answer;
-    }
-
 }
