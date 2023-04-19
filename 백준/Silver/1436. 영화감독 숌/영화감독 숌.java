7189d3fd27aaec2a2ad89bcd39c90d9e425edd00
sojeongLee <sojeonglee0125@gmail.com>
2023-04-19 21:13:12 +0900

    [Silver V] Title: 영화감독 숌, Time: 300 ms, Memory: 86000 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
@@ -3,16 +3,17 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 /**
- * Q. N번째 영화의 제목에 들어간 수를 출력하는 프로그램을 작성
+ * Q. 숌이 만든 N번째 영화의 제목에 들어간 수를 출력하는 프로그램을 작성하시오.
  * - 종말의 수란 어떤 수에 6이 적어도 3개 이상 연속으로 들어가는 수를 말한다.
- * - N번째 영화의 제목은 세상의 종말 (N번째로 작은 종말의 수) 와 같다.
+ * - N번째 영화의 제목은 세상의 종말 (N번째로 작은 종말의 수)와 같다.
+ * - 숌은 이 시리즈를 항상 차례대로 만들고, 다른 영화는 만들지 않는다.
  */
 public class Main {
     static int N, ans;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+        
         // 첫째 줄에 N이 주어진다. N은 10,000보다 작거나 같은 자연수이다.
         N = Integer.parseInt(br.readLine());
         
