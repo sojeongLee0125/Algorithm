1b295f542eef1bac9a1c6594a20df2c7c93e2cc1
sojeongLee <sojeonglee0125@gmail.com>
2023-05-01 14:29:50 +0900

    [level 4] Title: 징검다리, Time: 0.47 ms, Memory: 78.3 MB -BaekjoonHub

--- 프로그래머스/lv4/43236. 징검다리/징검다리.java
+++ 프로그래머스/lv4/43236. 징검다리/징검다리.java
@@ -1,16 +1,9 @@
 import java.util.*;
 
-// 출발지점부터 distance만큼 떨어진 곳에 도착지점이 있습니다. 그리고 그사이에는 바위들이 놓여있습니다. 
-// 바위 중 몇 개를 제거하려고 합니다.
-// 바위를 n개 제거한 뒤 각 지점 사이의 거리의 최솟값 중에 가장 큰 값을 return 하도록 solution 함수를 작성해주세요.
-
 class Solution {
     public int solution(int distance, int[] rocks, int n) {
         int answer = 0;
-        
-        // 범위 : 0 ~ distance;
-        // 이분탐색 방법 : mid 값을 가지고 해당 값을 최소값이라고 정했을 때 지워야 하는 바위수를 n과 비교한다.
-        
+                
         // 바위를 위치 순서대로 정렬한다.
         Arrays.sort(rocks);
         int lt = 0;
@@ -44,4 +37,4 @@ class Solution {
                          
         return answer;
     } // sol 메서드
-} //class
+}
