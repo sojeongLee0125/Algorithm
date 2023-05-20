48fd54469abc10c698d83a62ae6adf40a7e6275a
sojeongLee <sojeonglee0125@gmail.com>
2023-05-20 10:49:39 +0900

    [Gold V] Title: 자두나무, Time: 132 ms, Memory: 15292 KB -BaekjoonHub

--- 백준/Gold/2240. 자두나무/자두나무.java
+++ 백준/Gold/2240. 자두나무/자두나무.java
@@ -1,52 +1,53 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 /**
- * Q. 매 초마다 어느 나무에서 자두가 떨어질지에 대한 정보가 주어졌을 때, 자두가 받을 수 있는 자두의 개수를 구해내는 프로그램을 작성하시오.
+ * Q. 매 초마다 어느 나무에서 떨어질지에 대한 정보가 주어졌을 때, 자두가 받을 수 있는 자두의 개수 구하기.
+ * - 매 초마다, 두 개의 나무 중 하나의 나무에서 열매가 떨어지게 된다.
+ * - 만약 열매가 떨어지는 순간, 자두가 그 나무의 아래에 서 있으면 자두는 그 열매를 받아먹을 수 있다.
+ * - 두 개의 나무는 그다지 멀리 떨어져 있지 않기 때문에, 자두는 하나의 나무 아래에 서 있다가
+ * - 다른 나무 아래로 빠르게(1초보다 훨씬 짧은 시간에) 움직일 수 있다.
+ * - 하지만 자두는 체력이 그다지 좋지 못해서 많이 움직일 수는 없다.
+ * - 자두는 T(1≤T≤1,000)초 동안 떨어지게 된다. 자두는 최대 W(1≤W≤30)번만 움직이고 싶어 한다.
+ * - 자두는 1번 자두나무 아래에 위치해 있다고 한다.
  */
 public class Main {
-    static int T, W;
-    static int[] drop; // 각 시간마다 자두가 떨어지는 위치
-    static int[][][] dp = new int[1002][32][2]; // 시간, 움직인 횟수, 자두의 위치
+    static int T, W, MIN = -35;
+    static int[] arr;
+    static int[][][] dp = new int[1005][35][2];
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         // 첫째 줄에 두 정수 T, W가 주어진다.
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        T = Integer.parseInt(st.nextToken()); // 자두가 떨어지는 초
-        W = Integer.parseInt(st.nextToken()); // 자두가 움직일 수 있는 횟수
-        drop = new int[T];
+        T = Integer.parseInt(st.nextToken());
+        W = Integer.parseInt(st.nextToken());
 
         // 다음 T개의 줄에는 각 순간에 자두가 떨어지는 나무의 번호가 1 또는 2로 주어진다.
+        arr = new int[T];
+
         for (int i = 0; i < T; i++) {
-            drop[i] = Integer.parseInt(br.readLine()) - 1;
+            arr[i] = Integer.parseInt(br.readLine()) - 1;
         }
 
-        // dp 배열 초기화 : -1
-        for (int i = 0; i < dp.length; i++) {
-            for (int j = 0; j < dp[i].length; j++) {
-                Arrays.fill(dp[i][j], -1);
-            }
-        }
-
-        int answer = Math.max(go(0, W, 0), go(0, W - 1, 1));
-        System.out.println(answer);
+        // 첫째 줄에 자두가 받을 수 있는 자두의 최대 개수를 출력한다.
+        System.out.println(Math.max(move(0, 1, W - 1), move(0, 0, W)));
     }
 
-    private static int go(int cnt, int w, int cur) {
-        // 기저사례
-        if (w < 0) return Integer.MIN_VALUE; // 움직이는 횟수를 다 쓴 경우
-        if (cnt == T) return 0; // 끝에 도달했을 경우
+    private static int move(int time, int tree, int cnt) {
+        // 이동 횟수가 초과된 경우
+        if (cnt < 0) return MIN;
 
-        // 메모이제이션
-        if (dp[cnt][w][cur] != -1) return dp[cnt][w][cur];
+        // T초에 도달한 경우
+        if (time == T) return cnt == 0 ? 0 : MIN;
+        if (dp[time][cnt][tree] != 0) return dp[time][cnt][tree];
 
-        return dp[cnt][w][cur] = Math.max(go(cnt + 1, w - 1, ((cur + 1) % 2)),
-                go(cnt + 1, w, cur)) + (drop[cnt] == cur ? 1 : 0);
+        int ret = Math.max(move(time + 1, tree ^ 1, cnt - 1), move(time + 1, tree, cnt));
+        if (tree == arr[time]) ret++;
+
+        return dp[time][cnt][tree] = ret;
     }
-
 }
