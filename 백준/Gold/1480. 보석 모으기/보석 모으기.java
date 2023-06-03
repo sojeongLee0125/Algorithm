6e900bc449c46acb2c741a0bdf2e75be3f22c9b4
sojeongLee <sojeonglee0125@gmail.com>
2023-06-04 07:01:17 +0900

    [Gold I] Title: 보석 모으기, Time: 344 ms, Memory: 26792 KB -BaekjoonHub

--- 백준/Gold/1480. 보석 모으기/보석 모으기.java
+++ 백준/Gold/1480. 보석 모으기/보석 모으기.java
@@ -8,14 +8,13 @@ import java.util.StringTokenizer;
  * - 숌 보석상에 있는 모든 보석을 다 훔치려고 한다.
  * - 세준이는 보석을 다 가져올 수는 없다.
  * - 그 이유는 가방의 개수에 제한이 있고, 가방마다 넣을 수 있는 보석의 개수가 제한
- * - 세준이는 M개의 가방을 가지고 있다. 각각의 가방은 C그램의 보석을 담을 수 있다.
+ * - 세준이는 M개의 가방을 가지고 있다. 각각의 가방은 C 그램의 보석을 담을 수 있다.
  * - 숌 보석상에는 보석이 N개 있다.
  */
 public class Main {
-
     static int N, M, C;
     static int[] jewels;
-    static int[][][] dp = new int[1 << 15][25][25];
+    static int[][][] dp;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -28,8 +27,8 @@ public class Main {
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
         C = Integer.parseInt(st.nextToken());
-
         jewels = new int[N];
+        dp = new int[1 << N][M + 2][C + 2];
 
         // 둘째 줄에 보석의 무게가 하나씩 주어진다.
         // 보석의 무게는 1보다 크거나 같고, 20보다 작거나 같은 자연수이다.
@@ -38,31 +37,34 @@ public class Main {
             jewels[i] = Integer.parseInt(st.nextToken());
         }
 
+        // 첫째 줄에 세준이가 가져갈 수 있는 최대 보석의 개수를 출력한다.
         System.out.println(find(0, 0, C));
     }
 
-    private static int find(int cur, int isGet, int capacity) {
-        // 가방이 가득 찼을 경우
-        if (cur == M) return 0;
+    private static int find(int isGet, int bagIdx, int capacity) {
+        // 가방을 전부 순회할 경우
+        if (bagIdx == M) return 0;
 
-        if (dp[isGet][cur][capacity] != 0) return dp[isGet][cur][capacity];
+        // 메모이제이션
+        if (dp[isGet][bagIdx][capacity] != 0) return dp[isGet][bagIdx][capacity];
 
-        // 현재 훔치지 않을 경우
-        dp[isGet][cur][capacity] = Math.max(dp[isGet][cur][capacity], find(cur + 1, isGet, C));
+        // 새로운 가방으로 넘어가는 경우
+        dp[isGet][bagIdx][capacity] = Math.max(dp[isGet][bagIdx][capacity],
+                find(isGet, bagIdx + 1, C));
 
+        // 현재 가방에 보석을 훔칠 경우
         for (int i = 0; i < N; i++) {
-            int beforeGet = (1 << i) & isGet;
-            boolean canGet = (capacity - jewels[i]) >= 0;
-            if (beforeGet == 0 && canGet) {
-                dp[isGet][cur][capacity] = Math.max(
-                        dp[isGet][cur][capacity],
-                        find(cur, isGet | (1 << i), capacity - jewels[i]) + 1
+            boolean isAlreadyGet = ((1 << i) & isGet) != 0; // 이미 가지고 있는지 여부
+            boolean canGet = (capacity - jewels[i]) >= 0; // 무게 체크
+
+            if (!isAlreadyGet && canGet) {
+                dp[isGet][bagIdx][capacity] = Math.max(
+                        dp[isGet][bagIdx][capacity],
+                        find(isGet | (1 << i), bagIdx, capacity - jewels[i]) + 1
                 );
             }
         }
 
-        return dp[isGet][cur][capacity];
+        return dp[isGet][bagIdx][capacity];
     }
-
-
 }
